eddd85b9751103a95d1f0455f423128805744067
Kirill Kiselev <k.kiselev@octavianonline.com>
2019-08-21 18:14:52 +0300

    add sign up form

---
+++ src/main/java/com/cyrillwork/chart/SignUpUser.java
@@ -0,0 +1,9 @@
+package com.cyrillwork.chart;
+
+import lombok.Data;
+
+@Data
+public class SignUpUser {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/cyrillwork/chart/controllers/SignUpController.java
@@ -0,0 +1,30 @@
+package com.cyrillwork.chart.controllers;
+
+import com.cyrillwork.chart.SignUpUser;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/sign_up")
+public class SignUpController {
+
+    @GetMapping
+    public String showSignUpForm(Model model)
+    {
+        model.addAttribute("sign_up", new SignUpUser());
+        return "sign_up";
+    }
+
+    @PostMapping
+    public String processSignUp(SignUpUser signUpUser)
+    {
+        log.info("Sign up user" + signUpUser);
+        return "sign_up";
+    }
+
+}
