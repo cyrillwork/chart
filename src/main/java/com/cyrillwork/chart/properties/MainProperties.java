54913cd2d1681740a1c94246adee430693e5e8af
Kirill Kiselev <k.kiselev@octavianonline.com>
2019-09-30 11:38:12 +0300

    add depend os upload path

--- src/main/java/com/cyrillwork/chart/properties/MainProperties.java
+++ src/main/java/com/cyrillwork/chart/properties/MainProperties.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Properties;
+
 @Data
 @Configuration
 @ConfigurationProperties("server")
@@ -15,11 +17,25 @@ public class MainProperties {
     @Value("${chart.server.port}")
     private Integer port;
 
-    //@Value("${chart.server.greeting}")
-    //private String greeting;
+    @Value("${chart.upload.path.linux}")
+    private String uploadPathLinux;
 
-    @Value("${chart.upload.path}")
+    @Value("${chart.upload.path.windows}")
+    private String uploadPathWindows;
+
     private String uploadPath;
+
+    MainProperties(){
+        String property = System.getProperty("os.name");
+        switch (property)
+        {
+            case "Windows":
+                    uploadPath = uploadPathWindows;
+                break;
+            default:
+                uploadPath = uploadPathLinux;
+        }
+    }
 
     public boolean isLocalHost(){
         return host.equals("localhost");
